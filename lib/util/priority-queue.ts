881b8e782f457e173f253b716614947cc20193a1
adrnc <49120352+adrnc@users.noreply.github.com>
2024-05-06 16:11:23 +0200

    Add standard A* implementation

---
+++ lib/util/priority-queue.ts
@@ -0,0 +1,30 @@
+export class PriorityQueue<T> {
+    private readonly items: [T, number][] = [];
+    private isSorted = true;
+
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+
+    enqueue(item: T, priority: number): void {
+        this.items.push([item, priority]);
+        this.isSorted = false;
+    }
+
+    dequeue(): T {
+        if (!this.isSorted) {
+            // sort from largest to smallest
+            this.items.sort((a, b) => b[1] - a[1]);
+            this.isSorted = true;
+        }
+
+        const item = this.items.pop();
+
+        if (item === undefined) {
+            throw new Error("empty queue");
+        }
+
+
+        return item[0];
+    }
+}

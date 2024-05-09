fc93d78007131f122fa01f44130785956093a5c5
LuukEbenau <45255779+LuukEbenau@users.noreply.github.com>
2024-05-09 13:27:21 +0200

    added vector3Int class

--- lib/util/vectors.ts
+++ lib/util/vectors.ts
@@ -22,3 +22,23 @@ export class Vector2Int {
 		return new Vector2Int(this.x + v2.x, this.y + v2.y); // dont change the instance, since this will give sideeffects
 	}
 }
+
+export class Vector3Int extends Vector2Int{
+	public constructor(x:number, y:number,public readonly z:number){
+		super(x,y)
+	}
+
+	public equals(v2: Vector3Int): boolean {
+		return this.x === v2.x && this.y === v2.y;
+	}
+	public distance(v2:Vector3Int){
+		return Math.sqrt((v2.x - this.x) ** 2 + (v2.y - this.y) ** 2);
+	}
+	public static DEFAULT(): Vector3Int{
+		return new Vector3Int(0,0,0);
+	}
+
+	public add(v2:Vector3Int){
+		return new Vector3Int(this.x + v2.x, this.y + v2.y, this.z + v2.z); // dont change the instance, since this will give sideeffects
+	}
+}

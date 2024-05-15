f8e4a046a24419b728b67a442b8ee322f8f528e6
adrnc <49120352+adrnc@users.noreply.github.com>
2024-05-15 13:48:01 +0200

    Add if-then-else task

--- lib/behavior-tree/tasks.ts
+++ lib/behavior-tree/tasks.ts
@@ -30,6 +30,22 @@ export function not<TState, TAction, TConfig>(task: Task<TState, TAction, TConfi
     }
 }
 
+export function ite<TState, TAction, TConfig>(decisionTask: Task<TState, TAction, TConfig>, ifTask: Task<TState, TAction, TConfig>, elseTask?: Task<TState, TAction, TConfig>): Task<TState, TAction, TConfig> {
+    return (state, config, behaviorTree) => {
+        const decisionAction = decisionTask(state, config, behaviorTree);
+
+        if (decisionAction.status) {
+            return ifTask(state, config, behaviorTree);
+        }
+
+        if (elseTask === undefined) {
+            return new Action(false);
+        }
+
+        return elseTask(state, config, behaviorTree);
+    };
+}
+
 export function sequence<TState, TAction, TConfig>(...tasks: Task<TState, TAction, TConfig>[]): Task<TState, TAction, TConfig> {
     return (state, config, behaviorTree) => {
         let childAction = new Action<TAction>(true);
